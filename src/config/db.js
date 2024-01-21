964d7d913d689ea7b5e1f62e7893b24cbe0d7c60
tanwasp <tanishwasp10@gmail.com>
2024-01-21 09:21:38 -0500

    connected to azure database

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,22 @@
 import {Sequelize, Op} from 'sequelize';
 import dotenv from 'dotenv';
+import fs from 'fs';
+
 
 dotenv.config();
 
+const pathToCert = "DigiCertGlobalRootCA.crt.pem";
+
 // Database connection using Sequelize
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres'  //,
+  dialect: 'postgres' ,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: true, // This ensures that SSL is actually enforced
+      ca: fs.readFileSync(pathToCert).toString()
+    }
+  } 
   // logging: false
 });
 
